a559203b892bdc9a4503f91e461721017361ee34
GrauKatze <GrauKatze@protonmail.com>
2026-03-05 10:35:46 +0300

    refactor code

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,6 +1,3 @@
-
 fn main() {
     println!("Hello, extra!");
-    let _file_path = String::from("./extractor");
-    //check_file(&file_path);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,57 @@
 //! Help to extract any data from files
 
-
-pub struct ExHardDrive{
-    pub hd_size: u32,
+enum Config {
+    Version,
+    Help,
+    Command(Option<Vec<String>>),
 }
 
-pub trait IExtractor {
-    fn info(&self);
-    fn dump();
-    fn check_data();
-    fn search_delete_data();
-}
-
-impl IExtractor for ExHardDrive {
-    fn info(&self){
-        println!("size = {}",self.hd_size);
+impl Config {
+    fn build(init_options: &Option<Vec<String>>) -> Result<Config, String> {
+        match init_options {
+            None => Ok(Config::Help),
+            Some(options) => match options.get(0) {
+                Some(p) => match p.as_str() {
+                    "-h" | "--help" | "h" | "help" => Ok(Config::Help),
+                    "-v" | "--version" | "v" | "version" => Ok(Config::Version),
+                    _ => Err(format!(
+                        "not found this arguments\n{{ {:?} }}",
+                        options.iter().collect::<Vec<&String>>()
+                    )),
+                },
+                None => Err("Need args".to_string()),
+            },
+        }
     }
-    fn dump(){}
-    fn check_data(){}
-    fn search_delete_data(){}
+}
+pub struct Extractor {
+    configuration: Config,
+}
+
+impl Extractor {
+    pub fn build(init_options: &Option<Vec<String>>) -> Result<Extractor, String> {
+        let conf = Config::build(init_options);
+        match conf {
+            Ok(conf) => Ok(Extractor {
+                configuration: conf,
+            }),
+            Err(msg) => Err(msg),
+        }
+    }
+    pub fn run(&self) -> Result<(), String> {
+        match &self.configuration {
+            Config::Help => Self::write_help_msg(),
+            Config::Version => Self::write_version_msg(),
+            _ => Err("Error aanlis config".to_string()),
+        }
+    }
+    pub fn write_help_msg() -> Result<(), String> {
+        println!(env!("CARGO_PKG_NAME"));
+        println!(env!("CARGO_PKG_VERSION"));
+        Ok(())
+    }
+    pub fn write_version_msg() -> Result<(), String> {
+        println!(env!("CARGO_PKG_VERSION"));
+        Ok(())
+    }
 }
